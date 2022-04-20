3f01a6aa3a132db891c6172c5fd800c1c55cbc91
Tim Oleson <timothy.oleson@gmail.com>
2022-04-20 14:08:22 -0400

    Code Folder

---
+++ implement-message-workflows-with-service-bus/src/start/privatemessagesender/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace privatemessagesender
+{
+    class Program
+    {
+
+        const string ServiceBusConnectionString = "Endpoint=sb://salesteamappcontoso22.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=XAV90dAm6P/6+88zfL8Rh9ry/SHtQ7fc6yRSxXfM4co=";
+        const string QueueName = "salesmessages";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sending a message to the Sales Messages queue...");
+
+            SendSalesMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+        }
+
+        static async Task SendSalesMessageAsync()
+        {
+            // Create a Service Bus client here
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
+
+            // Create a sender here
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
+
+            try
+            {
+                // Create and send a message here
+
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the sender here
+
+            finally
+{
+            // Calling DisposeAsync on client types is required to ensure that network
+            // resources and other unmanaged objects are properly cleaned up.
+            await sender.DisposeAsync();
+            await client.DisposeAsync();
+}
+        }
+    }
+}
